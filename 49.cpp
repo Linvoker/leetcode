d2aef5ab65353ba89f20611e2408cb10232de628
llinvokerl <1959600040@qq.com>
2019-02-25 00:16:13 +0800

    49.cpp

--- 49.cpp
+++ 49.cpp
@@ -20,3 +20,23 @@ public:
         return ret_vec;
     }
 };
+//同类字符串如果不考虑字符出现的次数，则下面我的思路也是对的
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<int, vector<string>> resMap;
+        for (unsigned int i = 0; i < strs.size(); i++) {
+            int score = 0;
+            for (auto c : strs[i]) {
+                score |= 1 << (c - 'a'); //左移出一个唯一的整数作为key放入unordered_map
+            }
+            resMap[score].push_back(strs[i]);
+        }
+        vector<vector<string>> res{};
+        for (auto it = resMap.begin(); it != resMap.end(); it++) {
+            res.push_back((*it).second);
+        }
+        return res;
+    }
+};
+
